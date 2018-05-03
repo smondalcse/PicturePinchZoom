f03ec578c861cdec4ac405572e3fc64138dd1d04
20170546 <cse.sanat@gmail.com>
2018-05-03 11:48:51 +0600

    Initial commit

---
+++ app/src/main/java/com/sanat/nitolniloy/picturepinchzoom/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sanat.nitolniloy.picturepinchzoom;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.PhotoView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView mIcon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         mIcon = (ImageView) findViewById(R.id.ivIcon);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.off);
+        RoundedBitmapDrawable mDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        mDrawable.setCircular(true);
+        mIcon.setImageDrawable(mDrawable);
+
+        mIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_custom_layout, null);
+                PhotoView photoView = mView.findViewById(R.id.imageView);
+                photoView.setImageResource(R.drawable.off);
+                mBuilder.setView(mView);
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+            }
+        });
+    }
+}
